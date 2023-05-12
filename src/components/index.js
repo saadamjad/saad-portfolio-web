04ef22c701e061fb2a7cc168fa736b47d543eb8b
Muhammad Saad Amjad <46295251+saadamjad@users.noreply.github.com>
2023-05-13 03:58:49 +0500

    added some assets

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Button from './button'
-import {Loader} from './loader'
-const App = ()=>{
+import { Loader } from './loader';
 
-    return <div >
-    <Button/>
-    <Loader />
-  </div>
-}
+import '../App.css';
+
+const App = () => {
+	return (
+		<div className='childDiv'>
+			<p>
+				Edit <code>src/App.js</code> and save to reload.alsd,dla Edit{' '}
+				<code>src/App.js</code> and save to reload.alsd,dla Edit{' '}
+				<code>src/App.js</code> and save to reload.alsd,dla Edit{' '}
+				<code>src/App.js</code> and save to reload.alsd,dla Edit{' '}
+				<code>src/App.js</code> and save to reload.alsd,dla Edit{' '}
+				<code>src/App.js</code> and save to reload.alsd,dla Edit{' '}
+				<code>src/App.js</code> and save to reload.alsd,dla Edit{' '}
+				<code>src/App.js</code> and save to reload.alsd,dla
+			</p>
+		</div>
+	);
+};
 
 export default App
